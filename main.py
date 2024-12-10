b16aec78d01a414dfb229eba79f85bfb08cb5477
Khoa Nguyen <akhoa.nv@gmail.com>
2024-12-10 03:29:19 -0500

    Minor adjustments to parameters

--- main.py
+++ main.py
@@ -11,23 +11,32 @@ from FeatureExtractor.SuperPoint.SuperPoint import SuperPoint
 
 
 def main():
-    model_name = "bal"
-    extractor_params = {
-        'num_interest_points': 2000,
-        'ksize': 3,
-        'gaussian_size': 7,
-        'sigma': 6,
-        'alpha': 0.05,
-        'feature_width': 18,
-        'pyramid_level': 3,
-        'pyramid_scale_factor': 1.1
-    }
-    #extractor_params = {}
+    model_name = "model"
 
-    SFMRunner("test_data/tallneck2_mini", 6, extractor_params, feature_extractor_class=ScaleRotInvSIFT,
-              match_threshold=0.85, pose_estimator=PnPRansac, model_name=model_name, camera_sensor=SensorType.CROP_FRAME)
-    #SFMRunner("test_data/tallneck2_mini", 5, feature_extractor_class=SuperPoint, extractor_params=extractor_params,
-    #          match_threshold=0.85, pose_estimator=PnPRansac, camera_sensor=SensorType.CROP_FRAME, model_name=model_name)
+    #########################
+    # Naive SIFT
+    #########################
+    #extractor_params = {
+    #    'num_interest_points': 2500,
+    #    'ksize': 3,
+    #    'gaussian_size': 7,
+    #    'sigma': 6,
+    #    'alpha': 0.05,
+    #    'feature_width': 18,
+    #    'pyramid_level': 3,
+    #    'pyramid_scale_factor': 1.1
+    #}
+    #SFMRunner("test_data/tallneck2_mini", 10, extractor_params, feature_extractor_class=ScaleRotInvSIFT,
+    #          match_threshold=0.85, pose_estimator=PnPRansac, model_name=model_name, camera_sensor=SensorType.CROP_FRAME)
+
+    #########################
+    # SuperPoint
+    #########################
+    extractor_params = {}
+    SFMRunner("test_data/tallneck2_mini", 5, feature_extractor_class=SuperPoint, extractor_params=extractor_params,
+              match_threshold=0.85, pose_estimator=PnPRansac, camera_sensor=SensorType.CROP_FRAME, model_name=model_name)
+
+    # Load and Visualize
     SFMRunner.load(model_name)
     
 
